c67b4c05040d46f8d1f40ebd9fda93f886d2f315
dimell94 <dim.ell.094@gmail.com>
2025-05-13 20:53:54 +0300

    feat: add hello world

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * This project created to show how
+ * to make a git project. And then how
+ * to stage and commit changes
+ */
+
+public class HelloWorld {
+}
