73bd16e09c17957a64cfc75bcb9f33c331dc6450
dimell94 <dim.ell.094@gmail.com>
2025-05-15 21:40:21 +0300

    Fix: Added hello message to addapp

--- src/gr/aueb/cf/helloapp/AddApp.java
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -5,6 +5,7 @@ public class AddApp {
     public static void main(String[] args) {
         System.out.println(add(3,5));
         System.out.printf("%d", add(3, 5));
+        System.out.println("hello");
     }
 
     public static int add(int a, int b){
